892999b5eaddaabe426cbb5ebc511037e8b7c578
sockheadrps <r.p.skiles@gmail.com>
2023-08-13 23:07:38 -0400

    Changed socket event checking to use pattern matching, cleaned up formatting

--- browser/tts.js
+++ browser/tts.js
@@ -1,14 +1,18 @@
+let user
+let username_box_div
+let username_elm
 const socket = new WebSocket('ws://0.0.0.0:8181/');
-let speaking = false;
-let messageQueue = [];
-let playing = false
-
-let idle = document.getElementById("chatter_idle");
-let talking = document.getElementById("username");
 
 
-let user = document.getElementById("username")
-document.body.style.backgroundImage = "url('idle.png')";
+document.addEventListener('DOMContentLoaded', function () {
+    username_box_div = document.getElementById('box');
+    username_elm = document.getElementById("username");
+    user = document.getElementById("username")
+
+    document.body.style.backgroundImage = "url('idle.png')";
+    username_box_div.style.display = 'none';
+});
+
 
 socket.addEventListener('open', () => {
     content = document.getElementById('display');
@@ -23,26 +27,23 @@ socket.addEventListener('open', () => {
 });
 
 
-
 socket.addEventListener('message', async (event) => {
     const data = JSON.parse(event.data);
-    console.log("data is: ", data.event)
     if (!data.event) {
         return
-    }
-    if (data.event === "IS_SPEAKING"){
-        console.log(" if")
-        console.log(data)
-        talking.innerText = data.user
-        document.body.style.backgroundImage = "url('talking.png')";
-    }
-    else if (data.event ==='SPEAKING_COMPLETE') {
-        console.log("else if")
-        talking.innerText = ""
-        document.body.style.backgroundImage = "url('idle.png')";
-    }
-    else {
-        console.log('no....')
+    } else {
+        switch (data.event) {
+            case "IS_SPEAKING":
+                username_box_div.style.display = 'block';
+                username_elm.innerText = data.user
+                document.body.style.backgroundImage = "url('talking.png')";
+                break;
+
+            case 'SPEAKING_COMPLETE':
+                username_box_div.style.display = 'none';
+                username_elm.innerText = ""
+                document.body.style.backgroundImage = "url('idle.png')";
+        }
     }
 });
 
