e35c029d3e52bb0705b3dc52688b447ef3c4d5da
sockheadrps <r.p.skiles@gmail.com>
2024-08-26 19:39:23 -0400

    tabs to spaces conversion for github

--- TwitchBot/bot/cogs/tts_server.py
+++ TwitchBot/bot/cogs/tts_server.py
@@ -9,42 +9,42 @@ from pydantic_core._pydantic_core import ValidationError
 server_ip = "localhost"
 port = 8181
 clients = {
-	"TWITCHIO_CLIENT": None,
-	"OBS_CLIENT": None
+    "TWITCHIO_CLIENT": None,
+    "OBS_CLIENT": None
 }
 
 class Tts_server(commands.Cog):
-	def __init__(self, bot: Bot):
-		self.bot = bot
-		bot.loop.create_task(self.main())
+    def __init__(self, bot: Bot):
+        self.bot = bot
+        bot.loop.create_task(self.main())
 
-	async def main(self):
-		await serve(self.server, server_ip, port)
+    async def main(self):
+        await serve(self.server, server_ip, port)
 
-	async def server(self, websocket):
-		global clients
-		try:
-			async for message in websocket:
-				try:
-					validated_msg = Model(data=json.loads(message)).data
-					match validated_msg:
-						case ConnectTwitch(client=client) | ConnectOBS(client=client):
-							clients[client] = websocket
-							client_name = client
+    async def server(self, websocket):
+        global clients
+        try:
+            async for message in websocket:
+                try:
+                    validated_msg = Model(data=json.loads(message)).data
+                    match validated_msg:
+                        case ConnectTwitch(client=client) | ConnectOBS(client=client):
+                            clients[client] = websocket
+                            client_name = client
 
-						case IsSpeaking(user=user):
-							if clients['OBS_CLIENT'] is not None:
-								await clients['OBS_CLIENT'].send(validated_msg.model_dump_json())
+                        case IsSpeaking(user=user):
+                            if clients['OBS_CLIENT'] is not None:
+                                await clients['OBS_CLIENT'].send(validated_msg.model_dump_json())
 
-						case SpeakingComplete():
-							if clients['OBS_CLIENT'] is not None:
-								await clients['OBS_CLIENT'].send(validated_msg.model_dump_json())
+                        case SpeakingComplete():
+                            if clients['OBS_CLIENT'] is not None:
+                                await clients['OBS_CLIENT'].send(validated_msg.model_dump_json())
 
-				except ValidationError:
-					raise
+                except ValidationError:
+                    raise
 
-		except (ConnectionClosedOK, ConnectionClosedError):
-			clients[client_name] = None
+        except (ConnectionClosedOK, ConnectionClosedError):
+            clients[client_name] = None
 
 def prepare(bot: commands.Bot):
-	bot.add_cog(Tts_server(bot))
+    bot.add_cog(Tts_server(bot))
